d5b1801463d6b73b2a1822eba64267bf3f48e08a
Jasmit Lathiya <lathiyajasmit@gmail.com>
2026-04-07 19:02:43 +0530

    Scroll issue solved

--- src/organisms/ProjectCard.tsx
+++ src/organisms/ProjectCard.tsx
@@ -32,7 +32,7 @@ export default function ProjectCard({ project, index }: ProjectCardProps) {
       onClick={() => setIsModalOpen(true)}
       className="
         relative p-6 rounded-2xl bg-white dark:bg-white/5 border border-black/10 dark:border-white/10 
-        overflow-hidden cursor-pointer
+        cursor-pointer
         transition-shadow duration-300
         hover:shadow-2xl
       "
@@ -65,7 +65,7 @@ export default function ProjectCard({ project, index }: ProjectCardProps) {
       <AnimatePresence>
         {isModalOpen && (
           <motion.div
-            className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm cursor-default"
+            className="fixed inset-0 z-50 flex items-end md:items-center justify-center bg-black/50 backdrop-blur-sm cursor-default"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
@@ -73,38 +73,42 @@ export default function ProjectCard({ project, index }: ProjectCardProps) {
           >
             {/* Modal content */}
             <motion.div
-              className="bg-white dark:bg-gray-900 rounded-2xl p-6 max-w-lg w-full shadow-xl relative max-w-[90vw] max-h-[90vh] overflow-auto"
+              className="bg-white dark:bg-gray-900 rounded-2xl p-6 max-w-lg w-full shadow-xl relative h-full md:h-auto max-h-[90vh] w-full md:w-auto"
               initial={{ scale: 0.8, opacity: 0 }}
               animate={{ scale: 1, opacity: 1 }}
               exit={{ scale: 0.8, opacity: 0 }}
               onClick={(e) => e.stopPropagation()}
             >
-              <div className="flex flex-row justify-between items-center">
-                <h2 className="text-2xl font-bold mb-2">{project.title}</h2>
-                <button
-                  className="text-gray-500 hover:text-black dark:hover:text-white cursor-pointer"
-                  onClick={() => setIsModalOpen(false)}
-                >
-                  ✕
-                </button>
-              </div>
-              <p className="text-gray-600 dark:text-gray-400">{project.desc}</p>
-
-              {project.liveSiteURL && (
-                <p className="text-gray-600 dark:text-gray-400 mt-2">
-                  You can see live site here :{' '}
-                  <span
-                    className="text-blue-500 cursor-pointer"
-                    onClick={() => {
-                      window.open(`https://${project.liveSiteURL}`)
-                    }}
+              <div className="flex flex-col mb-4">
+                <div className="flex flex-row justify-between items-center">
+                  <h2 className="text-2xl font-bold mb-2">{project.title}</h2>
+                  <button
+                    className="text-gray-500 hover:text-black dark:hover:text-white cursor-pointer"
+                    onClick={() => setIsModalOpen(false)}
                   >
-                    {project.liveSiteURL}
-                  </span>
+                    ✕
+                  </button>
+                </div>
+                <p className="text-gray-600 dark:text-gray-400">
+                  {project.desc}
                 </p>
-              )}
+
+                {project.liveSiteURL && (
+                  <p className="text-gray-600 dark:text-gray-400 mt-2">
+                    You can see live site here :{' '}
+                    <span
+                      className="text-blue-500 cursor-pointer"
+                      onClick={() => {
+                        window.open(`https://${project.liveSiteURL}`)
+                      }}
+                    >
+                      {project.liveSiteURL}
+                    </span>
+                  </p>
+                )}
+              </div>
               {/* Explanation list */}
-              <ul className="list-disc pl-5 space-y-2 text-gray-600 dark:text-gray-400 max-h-[70vh] overflow-auto no-scrollbar mt-4 pt-4 border-t border-gray-300">
+              <ul className="list-disc pl-5 space-y-2 text-gray-600 dark:text-gray-400 max-h-[70vh] overflow-y-auto no-scrollbar pt-4 pb-16 md:pb-4 border-t border-gray-300">
                 {project.explanation?.map((item, i) => (
                   <li key={i}>{item}</li>
                 ))}
